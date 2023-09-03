508c75b6cbc6a7f42b5a2dd0cfe408d362dcf3a2
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-09-03 13:01:18 +0200

    crud provider final

--- src/app/update-provider/update-provider.component.ts
+++ src/app/update-provider/update-provider.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ProviderService } from './../services/provider.service';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-update-provider',
   templateUrl: './update-provider.component.html',
   styleUrls: ['./update-provider.component.css']
 })
-export class UpdateProviderComponent {
+export class UpdateProviderComponent implements OnInit {
+  id:any;
+  providerToUpdate:any;
+  name:any;
+  email:any;
+  adresse:any;
+
+  constructor(private service: ProviderService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(
+      params => {
+        this.id = params.get('id');
+      }
+    );
+    this.service.getProvider(this.id).subscribe(
+      (response: any) => {
+
+        this.name = response["name"];
+        this.email = response["email"];
+        this.adresse = response["adresse"];
+      }
+    );
+  }
+
+
+  updateProvider() {
+    this.providerToUpdate = {
+      "name": this.name,
+      "email": this.email,
+      "address": this.adresse,
+      "id": this.id
+    }
+    this.service.updateProvider(this.providerToUpdate).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['listProvider']);
+      }
+    );
+
+  }
 
 }
